e545a224ce3e1cdb404b75691ee04cdbb2edfcae
Daniel Pan <dprat0821@gmail.com>
2018-09-22 18:07:19 -0400

    init commit

---
+++ Music/Music/Music/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Daniel Pan on 2018-09-22.
+//  Copyright © 2018 geekmouse. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+      
+    }
+
+
+    
+}
+
